484bf3262667a26142b4cadae21959bfbfc712ad
Nichole Burton <nichole.p.burton@gmail.com>
2016-08-23 12:02:43 -0500

    add hamster data program

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,8 +1,31 @@
-hamster_name = nil
+#get hamster data
+
+puts "What is the hamster name? (string)"
+hamster_name = gets.chomp
+
+puts "What is the hamster volume level? (int)"
+volume_level = gets.to_i
+
+puts "What is the hamster's fur color? (string)"
+fur_color = gets.chomp
+
+puts "Is the hamster a good candidate? (y/n)"
+candidate_answer = gets.chomp
+if candidate_answer == "y"
+  good_candidate = true
+elsif candidate_answer == "n"
+  good_candidate = false
+end
+
+puts "How old is the hamster? (int)"
 #should be integer
-volume_level = nil
-fur_color = nil
-#should be boolean
-good_candidate = nil
-#should be integer
-hamster_age = nil
+hamster_age = gets.to_i
+if hamster_age == 0
+	hamster_age = nil
+end
+
+puts "Hamster name: #{hamster_name}"
+puts "Volume Level: #{volume_level}"
+puts "Fur color: #{fur_color}"
+puts "Good candidate for adoption? #{candidate_answer}"
+puts "Hamster age: #{hamster_age}"
