3ec30088a41588a0ae872def9fe18e48fdd9d6f2
Nichole Burton <nichole.p.burton@gmail.com>
2016-08-23 21:17:38 -0500

    add conditional logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -56,6 +56,22 @@ def check_age_accuracy(age_to_check, year_to_check)
 	end
 end
 
+# check the age and assign to variable for the following logic checks, for simplicity.
+verified_age = check_age_accuracy(age, birth_year)
+
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+if ((name == "Drake Cula") || (name == "Tu Fang"))
+puts "Definetly a vampire."
 # If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
-test_year = check_age_accuracy(age, birth_year)
-puts "#{test_year}"
+elsif verified_age && (garlic_bread || insurance)
+	puts "Probably not a vampire."
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+elsif !verified_age && (!garlic_bread || !insurance)
+	puts "Probably a vampire."
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+elsif !verified_age && !garlic_bread && !insurance
+puts "Almost certainly a vampire."
+
+else
+	"results inconclusive."
+end
