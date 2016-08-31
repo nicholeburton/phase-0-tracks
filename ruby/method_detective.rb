d0c32da5e04e0fb835cfc95f4f22a1646d5f6da9
Nichole Burton <nichole.p.burton@gmail.com>
2016-08-30 19:40:03 -0500

    add suspect list

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,10 +4,10 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".sub(/[o]/, 'oo')
 # => “zoom”
 
 # "enhance".<???>
@@ -32,7 +32,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
