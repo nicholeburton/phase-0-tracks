b0db3b77c7de89a08c6357c9fe1ff4c3792ba355
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-01 18:05:25 -0500

    swap spy first and last name.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+# Take spy name
+puts "What's the spy name?"
+
+#swap first and last name
+spy_name = gets.chomp
+
+spy_name.split(' ')
+(spy_name.reverse).
+#change all vowels to the next vowel and all consonants to next consonant in the alphabet.
+# convert to array
+
+#loop through away .each
+
+#if vowel, make next. if consonant, move to next consonant (not vowel)
+
+
+#put it back togtether.
