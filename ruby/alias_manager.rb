c7937ce3ce58f474be59a9f62dc1542159821df4
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-01 20:06:23 -0500

    initial commit with new file

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,17 +1,66 @@
-# Take spy name
-puts "What's the spy name?"
+# method to determine whether or not a letter is a vowel.
 
-#swap first and last name
-spy_name = gets.chomp
+$vowels = "aeiouAEIOU"
 
-spy_name.split(' ')
-(spy_name.reverse).
-#change all vowels to the next vowel and all consonants to next consonant in the alphabet.
-# convert to array
+def is_vowel(letter)
+  if ($vowels.include? letter)
+    return true
+  else
+    return false
+  end
+end
 
-#loop through away .each
+# turn the human into an awesome spy.
+def make_spy(spy_name)
+  #split up the spy name into words, reverse the words into array, join them together into string, then break them up as letters in array.
+  spy_name = spy_name.split(' ').reverse.join(' ').split('')
 
-#if vowel, make next. if consonant, move to next consonant (not vowel)
+  spy_name.map!{|letter|
+    # if it's a vowel, advance it to the next vowel.
+    if is_vowel(letter)
+        letter_index = $vowels.index(letter)
+        letter = $vowels[letter_index + 1]
+        # if the letter next to a consonant is a vowel, skip to the next one. There are no vowels next to each other so I'm not concerned about that.
+    elsif is_vowel(letter.next)
+        letter.next.next
+        # check for edge cases.
+    elsif letter == ' '
+        letter = ' '
+    elsif letter == 'z'
+        letter = "a"
+    elsif letter == 'Z'
+        letter = 'A'
+    else
+      # otherwise advance.
+        letter.next
+    end
+  }
 
+  spy_name = spy_name.join('')
+end
 
-#put it back togtether.
+def gather_spy_names
+  # declare hash in which to store values.
+  spy_collection = {}
+  spy_name = ''
+
+  # Take spy name
+  puts "What's the spy name? ('quit' to quit.)"
+
+  #swap first and last name
+
+  until (spy_name) == "quit"
+    spy_name = gets.chomp
+    if (spy_namme) != "quit"
+      spy_collection[:spy_name] = (make_spy(spy_name))
+    end
+  end
+
+  if spy_collection != {}
+    spy_collection.map{ |old_name, new_name|
+      puts "#{old_name} is now #{new_name}"
+    }
+  end
+end
+
+gather_spy_names
