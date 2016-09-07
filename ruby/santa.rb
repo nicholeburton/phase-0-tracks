3b5ccbfe01e4bc1048d65cafb0f444520da2ec1e
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-06 22:34:39 -0500

    Add initializations to Santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,45 @@
 class Santa
 
-  def initialize
-    @name = "Barry"
-    @gender = "gender non-conforming"
+  def initialize (ethnicity, gender)
+    @ethnicity = ethnicity
+    @gender = gender
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
-  def speak
-    puts "#{@name} says, 'Ho, ho ho! Haaaaaapy holidays!''"
-  end
-
-  def eat_milk_and_cookies (cookie_type)
-    puts "#{@name} says, 'That was a good #{cookie_type}!''"
-  end
+  # def speak
+  #   puts "#{@name} says, 'Ho, ho ho! Haaaaaapy holidays!''"
+  # end
+  #
+  # def eat_milk_and_cookies (cookie_type)
+  #   puts "#{@name} says, 'That was a good #{cookie_type}!''"
+  # end
+  #
+  # def land(location)
+  #   puts "successfully landed in #{location}"
+  #   @location = location
+  # end
+  #
+  # def about
+  #   puts "Name: #{@name}"
+  #   puts "Gender: #{@gender}"
+  #   puts "Location: #{@location}"
+  # end
 
 end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+# santa = Santa.new("Molly", "gender non-conforming")
+# santa.about
+# santa.speak
+# santa.eat_milk_and_cookies("Snickerdoodle")
+# santa.land("Paris")
+# santa.about
