417f1a6e48982cd805b643cc0c4060d7cd1303f6
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-07 19:17:21 -0500

    Add additional Puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,7 +5,26 @@ class Puppy
     toy
   end
 
+  def speak(times)
+    times.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_years = human_years * 7
+    p dog_years
+    dog_years
+  end
+
 end
 
 fido = Puppy.new
 fido.fetch("ball")
+fido.speak(5)
+fido.roll_over
+fido.dog_years(4)
