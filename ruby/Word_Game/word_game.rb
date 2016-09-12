a84125e9ef1eaca01ec8939d60ab0678cd9b4e02
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-11 22:54:03 -0500

    initial word check method

--- ruby/Word_Game/word_game.rb
+++ ruby/Word_Game/word_game.rb
@@ -18,37 +18,50 @@
 #   return the underscore string
 #   reduce count var
 
+# THE KEY TO IT ALL, Y'ALL.
+# FRANK
+# 01234
+# _____
+# _ _ _ _ _
+# 0 1 2 3 4
+# 0 2 4 6 8
+
+# assuming this isn't going to work for words with duplicate letters...
+
 class Word_Guess_Game
 
-  attr_accessor :word, :word_length, :guesses, :guess_status
+  attr_accessor :word, :word_length, :guesses_left, :guess_status, :word_expanded
 
   def initialize(word)
     @word = word
     @word_length = word.length
     @guesses_left = word.length
 
+    # guess_status is a set of user-readable underscores and letters. ex. "molly" will become "_ _ _ _ _" / "_ o _ _ _", etc.
     @guess_status = ""
-    @guess_status_smushed = ""
+    # guess_status_smushed is @guess_status without the spaces. "_ _ _ _ _" becomes "_____" used for comparison to @word.
+    @word_expanded = ""
 
     @word_length.times do |num|
       if num == @word_length - 1
         @guess_status << "_"
+        @word_expanded << "#{@word[num]}"
       else
         @guess_status << "_ "
+        @word_expanded << "#{@word[num]} "
       end
     end
+
   end
 
   def compare_to_word
-    #convert into smaller string
-    @guess_status_smushed = ""
-    @guess_status.each_char do |char|
-      if guess_status.even?
-        @guess_status_smushed << char
-    end
-
     #check against original
-    @word == @guess_status_smushed
+    @guess_status == @word_expanded
+  end
+
+  def reveal_char(char)
+    placement = (@word.index(char)*2)
+    @guess_status[placement] = char
   end
 
   def my_method
@@ -67,15 +80,27 @@ class Word_Guess_Game
       puts "Guess a letter. #{@guesses_left} guesses left."
       user_guess = gets.chomp
       if @word.include? "#{user_guess}"
-        
+        reveal_char(user_guess)
+        @guesses_left -= 1
+      elsif !@word.include? "#{user_guess}"
+        @guesses_left -=1
       end
+      p @guess_status
     end
   end
 
+  # if @guess_status_smushed == @word
+  #   puts "You won! #{@word} is the correct answer. Good job!"
+  # else
+  #   puts "You lost. try again."
+
 end
 
-my_game = Word_Guess_Game.new("molly")
-p my_game.word
-p my_game.word_length
-p my_game.guesses
-p my_game.guess_status
+my_game = Word_Guess_Game.new("frank")
+puts "#{my_game.word} word"
+puts "#{my_game.word_expanded} Expanded word"
+puts "#{my_game.guess_status} User readable guess status#"
+puts "#{my_game.word_length} Word length"
+puts "#{my_game.guesses_left} Guesses left"
+
+my_game.my_method
