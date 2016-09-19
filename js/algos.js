0f0ad546cdea69d30e5b0fa774524b85d66e6be8
Nichole Burton <nichole.p.burton@gmail.com>
2016-09-18 23:19:42 -0500

    pseudocode and def for longest_phrase

---
+++ js/algos.js
@@ -0,0 +1,19 @@
+function longest_phrase(phrase) {
+
+  // define variables to keep track of...
+  // ...the phrase that is currently longest.
+  var longest_phrase = phrase[0]
+  // assigning from the first entry in the array.
+
+  // for each item in the array
+  for (i = 0; i > phrase.length; i++) {
+  // if the length of the compared phrase is greater than the length of the longest phrase
+    if longest_phrase < phrase[i] {
+  // we assign the new phrase as the longest one
+    longest_phrase = phrase[i]
+    }
+  // otherwise we do nothing.
+  }
+  // return the longest phrase.
+  return longest_phrase
+}
